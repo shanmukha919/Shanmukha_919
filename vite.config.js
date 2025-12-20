f1a5b89e96e74ee52c1ba90a43cd5c9e207d5ba4
shanmukha919 <potturishanmukha919@gmail.com>
2025-12-20 20:21:23 +0530

    Fix layout responsiveness and overflow issues

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Shannu-919_Lakshmi/',   // EXACT repo name
+  base: '/',
   build: {
     outDir: 'docs'
   }
